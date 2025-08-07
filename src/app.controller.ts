7a5aaea4329281de5f92d6d7e6ecd9defe15cb4f
grzafnan <grzafnan@gmail.com>
2025-08-08 04:24:10 +0600

    refactor: remove unused AppService dependency from AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Res } from "@nestjs/common";
-import { AppService } from "./app.service";
 import { Response } from "express";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get("/")
   getHello(@Res() res: Response): void {
     res.render("home");
